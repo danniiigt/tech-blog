953209f6e57fbefbab52030f25a7f353b319bf86
danniiigt <danielgonzalezt13@gmail.com>
2023-09-12 15:36:50 +0200

    Fixed <Image /> error

--- app/(user)/post/[slug]/ImagenBanner.jsx
+++ app/(user)/post/[slug]/ImagenBanner.jsx
@@ -4,19 +4,21 @@ import { BlogReadTime } from "../../components/BlogReadTime";
 export const ImagenBanner = ({ imagen, estimatedTime }) => {
   return (
     <div className="relative">
-      <Image
-        src={imagen}
-        alt={"Banner"}
-        width={1024}
-        height={400}
-        className="
-          w-full
-          object-cover
-          object-center
-          h-[390px]
-          rounded-lg
+      {imagen && (
+        <Image
+          src={imagen}
+          alt={"Banner"}
+          width={1024}
+          height={400}
+          className="
+        w-full
+        object-cover
+        object-center
+        h-[390px]
+        rounded-lg
         "
-      />
+        />
+      )}
 
       <BlogReadTime estimatedTime={estimatedTime} />
     </div>
